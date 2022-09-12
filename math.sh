599fa6c80b2f00e4302220b75923e1360cf859bb
tinegagideon <tinegagideon@gmail.com>
2022-09-12 10:10:59 +0300

    math

---
+++ math.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+num1=40
+num2=10
+
+
+echo "the sum is:"
+expr $num1 + $num2
+echo "the difference is:"
+expr $num1 - $num2
+echo "the division is:"
+expr $num1 / $num2
+echo "the multiplication is:"
+expr $num1 \* $num2
