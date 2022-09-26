25dc21304670c484cf1ea9ae2469597a34887cea
tinegagideon <tinegagideon@gmail.com>
2022-09-26 20:33:48 +0300

    :recycle:update script

--- update_script.sh
+++ update_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+release_file=/etc/os-release
 
-if [ -d /ect/pacman.d ]
+if grep -q "Arch" $release_file
 then
    sudo pacman -Syu
 fi
 
-if [ -d /etc/apt ]
+if grep -q "debian" $release_file || grep -q "ubuntu" $release_file
+
 then
    sudo apt update
    sudo apt dist-upgrade
