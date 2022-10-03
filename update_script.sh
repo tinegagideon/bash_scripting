906f329e559d03eabcc2c966d2bb9e02add7b1d6
tinegagideon <tinegagideon@gmail.com>
2022-10-03 22:44:54 +0300

    :recycle:update script

--- update_script.sh
+++ update_script.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" $release_file
+
 then
-   sudo pacman -Syu
+   sudo pacman -Syu 1>>$logfile 2>>$errorlog
+   if [ $? -ne 0 ]
+   then
+      echo "An error occurred, please check the $errorlog file."
+   fi
+
 fi
 
 if grep -q "debian" $release_file || grep -q "ubuntu" $release_file
 
 then
-   sudo apt update
-   sudo apt dist-upgrade
+   sudo apt update 1>>$logfile  2>>$errorlog
+   if [ $? -ne 0 ]
+   then
+      echo "An error occurred, please check the $errorlog file."
+   fi
+
+   sudo apt dist-upgrade 1>>$logfile  2>>$errorlog
+
+   if [ $? -ne 0 ]
+   then
+      echo "An error occurred, please check the $errorlog file."
+   fi
+
 fi
